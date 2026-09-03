31e995bcca921907cbf0b85651c83a12104d0f96
WalterAltamirano <altamiranow4@gmail.com>
2026-09-03 16:50:02 -0300

    wip & refac: canion disparando staticamente, comentarios y limpiando codigo

--- entrega-2-altamirano-walter/entities/player/Player.gd
+++ entrega-2-altamirano-walter/entities/player/Player.gd
@@ -1,22 +1,24 @@
 extends Sprite2D
 
-#@onready var cannon:Sprite2D;
-
 var speed = 200 #Pixeles
 
 func _physics_process(delta):
-	# Manera optimizada
+	# Movimiento izq a der.
 	var direction_optimized:int = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-	
-	var mouse_position:Vector2 = get_global_mouse_position();
-	var origen:Vector2 = global_position; #Mi posicion como nodo global.
-	var direccion_vector:Vector2= (mouse_position - origen).normalized();
 	
 	var cannon = $Cannon;
 	#Manera compleja (Matematica de vectores)
-	var mouse_cannon_orientation:Vector2 = mouse_position - global_position;
+	var mouse_cannon_orientation:Vector2 = get_global_mouse_position() - global_position;
 	rotation = mouse_cannon_orientation.angle();
+	
+	#Delegacion a Godot
+	#look_at()
 	
 	if Input.is_action_just_pressed("fire"):
 		cannon.fire();
+		
 	position.x += direction_optimized * speed * delta
+	
+	#Anotaciones:
+		#var origen:Vector2 = global_position; Mi posicion como nodo global.
+		#@onready var cannon:Sprite2D; Evitar en loops.
--- entrega-2-altamirano-walter/entities/player/cannon.gd
+++ entrega-2-altamirano-walter/entities/player/cannon.gd
@@ -1,6 +1,12 @@
 extends Sprite2D
 
 @onready var fire_position:Marker2D = $FirePosition;
+@export var projectile_scene:PackedScene;
 
 func fire():
-	pass
+	var projectile_instance:Projectile = projectile_scene.instantiate(); #No existe mas instance()
+	fire_position.add_child(projectile_instance);
+	
+#Anotaciones:
+	#Position2D fue renombrado por Marker2D
+	#Usar preload() implica cambiar la ruta del recurso cuando se cambie su path.
