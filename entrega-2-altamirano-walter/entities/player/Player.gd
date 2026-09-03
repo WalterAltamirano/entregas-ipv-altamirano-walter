9211f487f62e8d1be9b652d94ff83bdfa3b26daa
WalterAltamirano <altamiranow4@gmail.com>
2026-09-03 16:20:03 -0300

    Canion agregado con rotacion en direccion del mouse y preparando balas

--- entrega-2-altamirano-walter/entities/player/Player.gd
+++ entrega-2-altamirano-walter/entities/player/Player.gd
@@ -1,18 +1,22 @@
-extends Sprite
+extends Sprite2D
+
+#@onready var cannon:Sprite2D;
 
 var speed = 200 #Pixeles
 
 func _physics_process(delta):
-	# Manera básica
-	var direction:int = 0
-	if Input.is_action_pressed("move_left"):
-		direction = -1
-	elif Input.is_action_pressed("move_right"):
-		direction = 1
-	
-	#position.x += direction * speed * delta
-	
 	# Manera optimizada
 	var direction_optimized:int = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	
+	var mouse_position:Vector2 = get_global_mouse_position();
+	var origen:Vector2 = global_position; #Mi posicion como nodo global.
+	var direccion_vector:Vector2= (mouse_position - origen).normalized();
+	
+	var cannon = $Cannon;
+	#Manera compleja (Matematica de vectores)
+	var mouse_cannon_orientation:Vector2 = mouse_position - global_position;
+	rotation = mouse_cannon_orientation.angle();
+	
+	if Input.is_action_just_pressed("fire"):
+		cannon.fire();
 	position.x += direction_optimized * speed * delta
---
+++ entrega-2-altamirano-walter/entities/player/cannon.gd
@@ -0,0 +1,6 @@
+extends Sprite2D
+
+@onready var fire_position:Marker2D = $FirePosition;
+
+func fire():
+	pass
