ad89f34f917b9c50b2b5845754cce720f7a18693
WalterAltamirano <altamiranow4@gmail.com>
2026-09-06 19:50:54 -0300

    wip: modificando ataque de torreta por deteccion de colision

--- entrega-3-altamirano-walter/entities/enemies/Turret.gd
+++ entrega-3-altamirano-walter/entities/enemies/Turret.gd
@@ -5,12 +5,11 @@ extends Sprite2D
 
 @export var projectile_scene: PackedScene
 
-var player: Node2D
+var target: Node2D;
 var projectile_container: Node
 
 func initialize(turret_pos: Vector2, player: Node2D, projectile_container: Node) -> void:
 	global_position = turret_pos
-	self.player = player
 	self.projectile_container = projectile_container
 	fire_timer.connect("timeout", fire_at_player)
 	fire_timer.start()
@@ -20,5 +19,5 @@ func fire_at_player() -> void:
 	proj_instance.initialize(
 		projectile_container,
 		fire_position.global_position,
-		fire_position.global_position.direction_to(player.global_position)
+		fire_position.global_position.direction_to(target.global_position)
 	)
