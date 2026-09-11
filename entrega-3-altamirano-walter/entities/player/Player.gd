3399a24f2365f2174b931789634aea499ca53699
WalterAltamirano <altamiranow4@gmail.com>
2026-09-11 00:30:42 -0300

    wip: Agrego paredesestaticas -- colision con rigid bodys

--- entrega-3-altamirano-walter/entities/player/Player.gd
+++ entrega-3-altamirano-walter/entities/player/Player.gd
@@ -6,6 +6,7 @@ extends CharacterBody2D
 @export var FRICTION_WEIGHT: float = 0.1;
 @export var JUMP_SPEED: float = -300.0;
 @export var GRAVITY: float = 10.0;
+@export var PUSH_FORCE: float = 80.0;
 var speed: Vector2 = Vector2.ZERO;
 var projectile_container: Node;
 
@@ -46,5 +47,9 @@ func _physics_process(delta: float) -> void:
 	handle_events();
 	velocity.y += GRAVITY;
 	move_and_slide();
+	for i in get_slide_collision_count():
+		var c = get_slide_collision(i);
+		if c.get_collider() is RigidBody2D:
+			c.get_collider().apply_central_impulse(-c.get_normal() * PUSH_FORCE);
 	#No es recomendable modificar la posicion. Usar la interfaz de Godot
 	#position += velocity * delta
